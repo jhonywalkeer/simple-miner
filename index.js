35ecc37238d88ea132f3da54dbfaf3c48a47bce8
Jhony Walker <60971382+JhonyWalker-pixel@users.noreply.github.com>
2021-07-28 22:22:19 -0300

    Add files

---
+++ index.js
@@ -0,0 +1,19 @@
+const { payloads } = require('./payloads.json')
+const { Worker } = require('worker_threads')
+
+const LEADING_ZEROES = 4
+const final = []
+let finishedWorkers = 0
+
+for (let payload of payloads) {
+  const worker = new Worker('./worker.js', { env: { LEADING_ZEROES } })
+  worker.once('message', (message) => {
+    final.push(message)
+    finishedWorkers++
+    if (finishedWorkers === payloads.length) console.log(final)
+  })
+  worker.on('error', console.error)
+
+  console.log(`Iniciando worker de ID ${worker.threadId} e enviando o payload "${payload}"`)
+  worker.postMessage(payload)
+}
